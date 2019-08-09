383446a3633d5266771c09d38a6e06a23a6eeef7
coinlawyer <41735680+coinlawyer@users.noreply.github.com>
2019-08-09 16:20:06 +0300

    Not ready yet!

--- js/index.js
+++ js/index.js
@@ -1,55 +1,19 @@
-// for (let i=0; i<=5; i++){
-//     const para = document.createElement('p');
-//     para.textContent = i + '. Sample text';
-//     para.classList.add('highlight');
-//     sect.appendChild(para);
-// }
-
-// sect.appendChild(linkPara.cloneNode(true));
-// //sect.removeChild(linkPara);
-
-//Clock
-
-// new HoverIntent({
-//   elem,
-//   over() {
-//     tooltip.style.left = elem.getBoundingClientRect().left + 'px';
-//     tooltip.style.top = elem.getBoundingClientRect().bottom + 5 + 'px';
-//     document.body.append(tooltip);
-//   },
-//   out() {
-//     tooltip.remove();
-//   }
-// });
-
-const clock = document.querySelector('.clock');
-// const textTime = document.querySelector(' - Lviv local time');
-// clock.appendChild(textTime);
-// clock.addEventListener(mousein, textTimeAppear);  
-//     function textTimeAppear(e) {
-
-setInterval(
-    () => clock.innerText = (new Date()).toLocaleTimeString(),
-    1000);
-
 
 //Calculator
-// + 1-to clear display after errror notice by click on any other btn of digitsOper
-// + 2-to add clearLastEl button
-// + 3-to change to opposite operator by click on it if the last element is operator.
+//      + 1-to clear display after errror notice by click on any other btn of digitsOper
+//      + 2-to add clearLastEl button
+//      + 3-to change to opposite operator by click on it if the last element is operator.
 // 4-to add memory button
-// 5-to deny add '0' when the last elem == '0'
+//      + 5-to deny add '0' when the last elem == '0'
 // 6-to fix % funtion  
 // 7-to fix multiple usage of '.'
+//.... and many others...
 
 const display = document.querySelector('.display');
-
-
 const errorMessage = 'You can`t divide by "0"!';
 const operButtons = document.querySelectorAll('.operations button');
-operButtons.forEach(digit => digit.addEventListener('click', changeOperButtons));
-
-
+operButtons.forEach(digit => 
+    digit.addEventListener('click', changeOperButtons));
 
 function changeOperButtons(e){
     const lastChar = display.value.slice(-1);// or .replace(/.$/," ") or string.substring (from, to)
@@ -60,43 +24,29 @@ function changeOperButtons(e){
         }
     }
 }
-    // Array.includes = function() {
-    //     let [first, ...rest] = arguments;
-    //     return Array.prototype.includes.apply(first, rest);
-    //   }
-// } 
-
-function clearDisplay(display) {
-    e.preventDefault();
-    display.value = '';
-}
-
 const digitsOper = document.querySelectorAll('.digits button, .operations button'); // all buttons are assigned to the arr digitsOper[]
-digitsOper.forEach(digit => digit.addEventListener('click', digitOperPressed));
+digitsOper.forEach(digit => 
+    digit.addEventListener('click', digitOperPressed));
 
 function digitOperPressed(e) {
     e.preventDefault(); //prevent from sending form to server
-    
     const lastChar = display.value.slice(-1);// or .replace(/.$/," ") or string.substring (from, to)
-    
-    const IsOper = function(e){
-        for (let i=0; i<operButtons.length; i++) {
-            if (operButtons[i].innerText.includes(lastChar)) {
-                return IsOper = true;
-            }
-        }
-    }
-    if (display.value !== errorMessage){ 
-        display.value += e.target.innerText;
-    } 
-    else if (isOper(lastChar)&&isOper(e.target.innerText)) {
-            display.value = display.value(value.length - 1);
-    }
-    else {
-        display.value = e.target.innerText; //target is the clicked button 
+    switch(display.value) {
+        case(errorMessage):  
+        case('undefined'):
+        case(isNaN(display.value)): 
+            display.value = e.target.innerText;
+            break;
+        case ('0'):
+            display.value = display.value.slice(0, -1); 
+            break;
+        case ((display.value.includes('.'))&&(e.target.innerText==='.')):
+                display = display.value.slice(0, -1);
+                break;
+        default:
+            display.value += e.target.innerText; //target is the clicked button 
     }
 }
-
 const equal = document.querySelector('.equalBtn');
 equal.addEventListener('click', equalPressed);
 
@@ -128,8 +78,15 @@ percent.addEventListener('click', percentPressed);
 
 function percentPressed(e) {
     e.preventDefault();
-    display.value.scile(-2, -1) === operations.button.innerText ?
+    display.value.scile(-2, -1) === operButtons.innerText ?
         display.value = eval(display.value.scile(-1) / 100) :
         display.value = (eval(display.value)) / 100;
 }
 
+    // const IsOper = function(e){
+    //     for (let i=0; i<operButtons.length; i++) {
+    //         if (operButtons[i].innerText.includes(lastChar)) {
+    //             return IsOper = true;
+    //         }
+    //     }
+    // }
